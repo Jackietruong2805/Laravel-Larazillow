4bb3e40e6c27b775f32603ba583fe970a9441b10
Jackietruong2805 <truonghao2805@gmail.com>
2023-03-24 10:25:19 +0700

    Add Persistent Layout

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Index</h1>
+    <Link  href="/hello">Show Page</Link>
+    <div class="">
+        The message is {{ message }}
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    message: String
+});
+
+</script>
+
+<script>
+import MainLayout from '../../Layouts/MainLayout.vue';
+export default{
+    layout: MainLayout
+}
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Show</h1>
+    <Link  href="/">Index Page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<script>
+import MainLayout from '../../Layouts/MainLayout.vue';
+export default{
+    layout: MainLayout
+}
+
+</script>
